d42b7234ad9967015e2d2a5b8a635ca48da3fe95
Tommy Kneetz <tommy.kneetz@pcg.io>
2024-04-24 10:30:40 +0200

    ok

--- new-teams-disable-keep-running-on-close.ps1
+++ new-teams-disable-keep-running-on-close.ps1
@@ -9,8 +9,6 @@ $FileContent=Get-Content -Path "$ENV:localAPPDATA\packages\MSTeams_8wekyb3d8bbwe
 # Convert file content from JSON format to PowerShell object
 $JSONObject=ConvertFrom-Json -InputObject $FileContent
 # Update Object settings
-#$JSONObject.appPreferenceSettings.OpenAsHidden=$OpenAsHidden
-$JSONObject.user_modified_auto_start=$user_modified_auto_start
 $JSONObject.keep_app_running_on_close=$RunningOnClose
 # Terminate Teams Process
 Get-Process ms-Teams | Stop-Process -Force
